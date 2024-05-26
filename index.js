cf6052f651be9a8dc0e6c414f69fb3c0e0474745
eattonton <eattonton@qq.com>
2024-05-26 20:46:37 +0800

    B

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11778" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4358" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
